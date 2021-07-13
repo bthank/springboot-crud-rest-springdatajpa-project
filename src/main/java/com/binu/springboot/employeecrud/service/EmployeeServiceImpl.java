6084fa3ac6afbbe6abdd5a8d2fccfd3b9c522850
bthank <bthankachan@yahoo.com>
2021-07-13 17:37:52 -0400

    added find all and find by id functionality

--- src/main/java/com/binu/springboot/employeecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/binu/springboot/employeecrud/service/EmployeeServiceImpl.java
@@ -1,59 +1,67 @@
 package com.binu.springboot.employeecrud.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.binu.springboot.employeecrud.dao.EmployeeDAO;
+import com.binu.springboot.employeecrud.dao.EmployeeRepository;
 import com.binu.springboot.employeecrud.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	 
-	EmployeeDAO employeeDAO;
+	EmployeeRepository employeeRepository;
 	
 	@Autowired         // note that the bean id in the @Qualifier must start with a lower case letter
-	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
-		this.employeeDAO = theEmployeeDAO;
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
 	}
 	
 	@Override
-	@Transactional
+	//no need for using @Transactional since JPA Repository provides the same functionality automatically
 	public List<Employee> findAll() {
 		 
-		return employeeDAO.findAll();
+		return employeeRepository.findAll();
 	}
 
 	@Override
-	@Transactional
+	//no need for using @Transactional since JPA Repository provides the same functionality automatically
 	public Employee findById(int theId) {
 		 
-		return employeeDAO.findById(theId);
+		Optional<Employee> result = employeeRepository.findById(theId);
+		Employee theEmployee = null;
+		
+		if (result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			throw new RuntimeException("Did not find employee with id: " + theId);
+		}
+		return theEmployee;
 	}
 
 	@Override
-	@Transactional
+	//no need for using @Transactional since JPA Repository provides the same functionality automatically
 	public void save(Employee theEmployee) {
 		 
-		employeeDAO.save(theEmployee);
+		employeeRepository.save(theEmployee);
 	}
 
 	@Override
-	@Transactional
+	//no need for using @Transactional since JPA Repository provides the same functionality automatically
 	public void update(Employee theEmployee) {
 		 
-		employeeDAO.update(theEmployee);
+		employeeRepository.save(theEmployee);
 	}
 
 	@Override
-	@Transactional
+	//no need for using @Transactional since JPA Repository provides the same functionality automatically
 	public void deleteById(int theId) {
 		 
-		employeeDAO.deleteById(theId);
+		employeeRepository.deleteById(theId);
 	}
 
 }
